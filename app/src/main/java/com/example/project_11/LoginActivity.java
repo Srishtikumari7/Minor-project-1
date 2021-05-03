e9b1a1b64c929b46b2b5127ec9649cea5ef7dc32
Srishti <srishtikumari.mca19@rvce.edu.in>
2021-05-03 12:12:24 +0530

    Login and registration

---
+++ app/src/main/java/com/example/project_11/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.project_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageButton face,google;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn=findViewById(R.id.textviewSignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+        /*face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/"));
+                startActivity(i);
+            }
+        });*/
+    }
+}
